7b726f5c43fd92a0686a6e756d2c984565795877
Stefan Alpers <stefan.alpers@gmx.net>
2018-12-27 22:58:54 +0100

    Initial code without any tests.

---
+++ IncreasingTestabilityStepByStep/IncreasingTestabilityStepByStep/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IncreasingTestabilityStepByStep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter text:");
+            var text = Console.ReadLine();
+            var words = text.Split(' ');
+            var n = words.Length;
+            Console.WriteLine($"Number of words: {n}");
+
+            Console.ReadKey();
+        }
+    }
+}
