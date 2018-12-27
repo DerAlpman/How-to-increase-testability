9c725de80842a8ed5a3dabe0e7c5d619fe431964
Stefan Alpers <stefan.alpers@gmx.net>
2018-12-27 23:41:09 +0100

    First test

--- IncreasingTestabilityStepByStep/IncreasingTestabilityStepByStep/Program.cs
+++ IncreasingTestabilityStepByStep/IncreasingTestabilityStepByStep/Program.cs
@@ -1,22 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace IncreasingTestabilityStepByStep
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.Write("Enter text:");
-            var text = Console.ReadLine();
-            var words = text.Split(' ');
-            var n = words.Length;
+            Console.Write($"Enter text:");
+            string text = Console.ReadLine();
+            string[] words = text.Split(' ');
+            int n = words.Length;
             Console.WriteLine($"Number of words: {n}");
 
-            Console.ReadKey();
+            //Console.ReadKey();
         }
     }
 }
---
+++ IncreasingTestabilityStepByStep/IncreasingTestabilityStepByStepTests/UnitTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using IncreasingTestabilityStepByStep;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IncreasingTestabilityStepByStepTests
+{
+    [TestClass]
+    public class UnitTests
+    {
+        [TestMethod]
+        public void TestMain()
+        {
+            StringWriter output = new StringWriter();
+
+            Console.SetOut(output);
+            Console.SetIn(new StringReader("Mary had a little lamb"));
+
+            Program.Main(null);
+
+            Assert.AreEqual("Enter text:Number of words: 5\r\n", output.ToString());
+        }
+    }
+}
