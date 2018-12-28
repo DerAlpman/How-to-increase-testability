22db4ea7917610a0d1dd590baf37a1f3429916e5
Stefan Alpers <stefan.alpers@gmx.net>
2018-12-28 17:11:50 +0100

    One first grade of testability can be derived: code is automatically testable if it has no dependency to the UI.
    In this example we had dependencies to the UI in lines 9 to 10 and line 12.
    
    To separate logic that is easy to test from the difficult one, we move them to separate functions.
    
    But the overall testability of the behaviour has not improved, yet. Likewise we cannot explicitly test the domain logic (the counting of words) because it is caught between die UI method calls. This is a contradiction to the "Single Level of Abstraction" principle (http://principles-wiki.net/principles:single_level_of_abstraction).
    
    Lines 9 and 12 have now abstraction level 3 (logic behind a function name), lines 10 and 11 have abstraction level 2 ( logic named with a variable name).

--- IncreasingTestabilityStepByStep/IncreasingTestabilityStepByStep/Program.cs
+++ IncreasingTestabilityStepByStep/IncreasingTestabilityStepByStep/Program.cs
@@ -6,11 +6,22 @@ namespace IncreasingTestabilityStepByStep
     {
         public static void Main(string[] args)
         {
-            Console.Write($"Enter text:");
-            string text = Console.ReadLine();
+            string text = AskForText();
             string[] words = text.Split(' ');
             int n = words.Length;
+            DisplayWordCount(n);
+        }
+
+        public static void DisplayWordCount(int n)
+        {
             Console.WriteLine($"Number of words: {n}");
+        }
+
+        public static string AskForText()
+        {
+            Console.Write($"Enter text:");
+            string text = Console.ReadLine();
+            return text;
         }
     }
 }
